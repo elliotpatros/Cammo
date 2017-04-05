474c68d9cecccd67e7843a81026c39b3248e4dfc
Elliot Patros <empatros@gmail.com>
2017-04-04 20:44:28 -0700

    fixed crash on startup bug. that was super embarrassing...

---
+++ app/src/main/java/com/emp/cammo/FragmentMainMenu.java
@@ -0,0 +1,30 @@
+package com.emp.cammo;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentMainMenu extends Fragment {
+
+    public static FragmentMainMenu newInstance() {
+        return new FragmentMainMenu();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main_menu, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        MainActivity activity = (MainActivity) getActivity();
+        if (null == activity) return;
+
+        view.findViewById(R.id.goto_calibration).setOnClickListener(activity);
+        view.findViewById(R.id.goto_tracking).setOnClickListener(activity);
+    }
+}
