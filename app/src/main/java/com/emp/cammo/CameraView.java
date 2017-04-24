1119d9e1742086d909605a39d648168034a36924
Elliot Patros <empatros@gmail.com>
2017-04-24 16:13:19 -0700

    can reliably turn on front camera

--- app/src/main/java/com/emp/cammo/CameraView.java
+++ app/src/main/java/com/emp/cammo/CameraView.java
@@ -1,28 +1,67 @@
 package com.emp.cammo;
 
 import android.content.Context;
+import android.hardware.Camera;
 import android.util.AttributeSet;
+import android.view.SurfaceHolder;
 
+import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.JavaCameraView;
 
+import java.util.List;
+
 public class CameraView extends JavaCameraView {
+    final static private String TAG = "CameraView";
+
     // constructor
     public CameraView(Context context, AttributeSet attributeSet) {
         super(context, attributeSet);
     }
 
     @Override
-    protected boolean initializeCamera(int width, int height) {
-        return super.initializeCamera(width, height);
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        disconnectCamera();
     }
 
-    @Override
-    public void setCameraIndex(int cameraIndex) {
-        if (cameraIndex != mCameraIndex) {
-            enableView();
-            disableView();
-            super.setCameraIndex(cameraIndex);
-            enableView();
+    public void disconnect() {
+        disconnectCamera();
+    }
+
+    public void connect() {
+        if (null != mCamera) {
+            final Camera.Size preferredSize = mCamera.getParameters().getPreferredPreviewSizeForVideo();
+
+            Camera.Parameters parameters = mCamera.getParameters();
+            List<Camera.Size> previewSizes = parameters.getSupportedPreviewSizes();
+            Camera.Size previewSize = getBestPreviewSize(previewSizes, preferredSize.width, preferredSize.height);
+
+            connectCamera(previewSize.width, previewSize.height);
         }
     }
+
+    public boolean isConnected() {
+        return mCamera != null;
+    }
+
+    public void setErrorCallback(Camera.ErrorCallback callback) {
+        if (null != mCamera) {
+            mCamera.setErrorCallback(callback);
+        }
+    }
+
+    private Camera.Size getBestPreviewSize(List<Camera.Size> sizes, int w, int h) {
+        Camera.Size bestSize = null;
+        int bestError = 9999999;
+
+        for (Camera.Size size : sizes) {
+            int error = Math.abs(w - size.width) + Math.abs(h - size.height);
+            if (error < bestError) {
+                bestSize = size;
+                bestError = error;
+            }
+        }
+
+        return bestSize;
+    }
 }
+
--- app/src/main/java/com/emp/cammo/FragmentTracking.java
+++ app/src/main/java/com/emp/cammo/FragmentTracking.java
@@ -28,21 +28,22 @@ import org.opencv.core.Point;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 
+public class FragmentTracking extends Fragment implements CameraBridgeViewBase.CvCameraViewListener2, Camera.ErrorCallback {
 
-public class FragmentTracking extends Fragment implements CameraBridgeViewBase.CvCameraViewListener2 {
+    private final static String TAG = "FragmentTracking";
 
     // member variables
-    public int mCameraIndex = Camera.CameraInfo.CAMERA_FACING_FRONT;
+    public int mCameraIndex = CameraBridgeViewBase.CAMERA_ID_FRONT;
     private Mat mMatRgba; // color image of current frame given by CameraView
     private Mat mMatGray; // gray  image of current frame given by CameraView
     private final static int mFindFlags = Calib3d.CALIB_CB_FAST_CHECK;
 
     // face detector
-    FaceDetector mFaceDetector = null;
-    SparseArray<Face> mFaces = null;
-    Bitmap mBitmap = null;
-    Scalar mColor = new Scalar(0, 255, 0);
-    final static int mScale = 8;
+    private FaceDetector mFaceDetector = null;
+    private SparseArray<Face> mFaces = null;
+    private Bitmap mBitmap = null;
+    final private Scalar mColor = new Scalar(0, 255, 0);
+    final private static int mScale = 8;
 
     // widgets
     private CameraView mCameraView = null;
@@ -70,25 +71,15 @@ public class FragmentTracking extends Fragment implements CameraBridgeViewBase.C
 
         // setup widgets
         mCameraView = (CameraView) view.findViewById(R.id.cameraView);
+        mCameraView.setCameraIndex(mCameraIndex); // update camera index
     }
 
     @Override
     public void onResume() {
         super.onResume();
 
-        // setup mCamera view widget
-        if (null != mCameraView) {
-            mCameraView.setCvCameraViewListener(this);  // set listener for mCamera view callbacks
-            mCameraView.enableView();                   // turn mCamera stream on
-            mCameraView.setVisibility(View.VISIBLE);    // set widget visibility on
-
-            if (Camera.CameraInfo.CAMERA_FACING_BACK != mCameraIndex) {
-                mCameraView.setCameraIndex(mCameraIndex);
-            }
-        }
-
-        // done
-        setRetainInstance(true);
+        stopCamera();
+        startCamera();
 
         // keep window on
         MainActivity parent = (MainActivity) getActivity();
@@ -100,19 +91,13 @@ public class FragmentTracking extends Fragment implements CameraBridgeViewBase.C
     @Override
     public void onDestroy() {
         super.onDestroy();
-        if (null != mCameraView) {
-            mCameraView.disableView();
-            mCameraView.setVisibility(View.GONE);
-        }
+        stopCamera();
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        if (null != mCameraView) {
-            mCameraView.disableView();
-            mCameraView.setVisibility(View.GONE);
-        }
+        stopCamera();
     }
 
     @Override
@@ -123,6 +108,9 @@ public class FragmentTracking extends Fragment implements CameraBridgeViewBase.C
     // mCamera view callbacks
     @Override
     public void onCameraViewStarted(int width, int height) { // size of stream or preview?
+        Log.i(TAG, "onCameraViewStarted");
+        mCameraView.setErrorCallback(this);
+
         // setup image buffer just once before streaming
         mMatRgba = new Mat(height, width, CvType.CV_8UC4);
         mMatGray = new Mat(height/mScale, width/mScale, CvType.CV_8UC1);
@@ -133,60 +121,104 @@ public class FragmentTracking extends Fragment implements CameraBridgeViewBase.C
                 .setClassificationType(FaceDetector.ALL_CLASSIFICATIONS)
                 .setMode(FaceDetector.ACCURATE_MODE)
                 .build();
-//        mFaceDetector.setProcessor(
-//                new MultiProcessor.Builder<>(new GraphicFaceTrackerFactory()).build());
-        // checkout https://github.com/googlesamples/android-vision/blob/master/visionSamples/FaceTracker/app/src/main/java/com/google/android/gms/samples/vision/face/facetracker/FaceTrackerActivity.java
 
         mFaces = new SparseArray<>(1);
         mBitmap = Bitmap.createBitmap(width/mScale, height/mScale, Bitmap.Config.ARGB_8888);
+
+        Log.i(TAG, "onCameraViewStarted: " + mCameraView.isConnected());
     }
 
     @Override
     public void onCameraViewStopped() {
-        // 'free' image buffer after streaming
-        mMatRgba.release();
-        mFaceDetector.release();
+        Log.i(TAG, "onCameraViewStopped:");
+        stopCamera();
     }
 
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame frame) {
-        // get color frame from mCamera
-        mMatRgba = frame.rgba();
-        Imgproc.resize(frame.gray(), mMatGray, mMatGray.size(), 0, 0, Imgproc.INTER_AREA);
+        try {
+            // get color frame from mCamera
+            mMatRgba = frame.rgba();
+            Imgproc.resize(frame.gray(), mMatGray, mMatGray.size(), 0, 0, Imgproc.INTER_AREA);
 
-        // front mCamera is flipped, fix that here if it's active
-        if (Camera.CameraInfo.CAMERA_FACING_FRONT == mCameraIndex) {
-            Core.flip(mMatRgba, mMatRgba, Core.ROTATE_180);
-            Core.flip(mMatGray, mMatGray, Core.ROTATE_180);
-        }
-
-        if (mFaceDetector.isOperational()) {
-            // convert Mat to Frame
-            Utils.matToBitmap(mMatGray, mBitmap);
-            Frame faceFrame = new Frame.Builder().setBitmap(mBitmap).build();
-
-            // detect face
-            mFaces = mFaceDetector.detect(faceFrame);
-
-            if (0 < mFaces.size()) {
-                final Face face = mFaces.valueAt(0);
-                PointF pointf = face.getPosition();
-                pointf.x *= mScale;
-                pointf.y *= mScale;
-                final float w = face.getWidth() * mScale;
-                final float h = face.getHeight() * mScale;
-                Imgproc.rectangle(
-                        mMatRgba,
-                        new Point(pointf.x, pointf.y),
-                        new Point(pointf.x + w, pointf.y + h),
-                        mColor);
-
-                Log.i("onCameraFrame", String.valueOf(face.getEulerY()));
+            // front mCamera is flipped, fix that here if it's active
+            if (CameraBridgeViewBase.CAMERA_ID_FRONT == mCameraIndex) {
+                Core.flip(mMatRgba, mMatRgba, Core.ROTATE_180);
+                Core.flip(mMatGray, mMatGray, Core.ROTATE_180);
             }
-        }
 
-        // return the image we want to preview
-        return mMatRgba;
+            if (mFaceDetector.isOperational()) {
+                // convert Mat to Frame
+                Utils.matToBitmap(mMatGray, mBitmap);
+                Frame faceFrame = new Frame.Builder().setBitmap(mBitmap).build();
+
+                // detect face
+                mFaces = mFaceDetector.detect(faceFrame);
+
+                if (0 < mFaces.size()) {
+                    final Face face = mFaces.valueAt(0);
+                    PointF pointf = face.getPosition();
+                    pointf.x *= mScale;
+                    pointf.y *= mScale;
+                    final float w = face.getWidth() * mScale;
+                    final float h = face.getHeight() * mScale;
+                    Imgproc.rectangle(
+                            mMatRgba,
+                            new Point(pointf.x, pointf.y),
+                            new Point(pointf.x + w, pointf.y + h),
+                            mColor);
+
+//                Log.i("onCameraFrame", String.valueOf(face.getEulerY()));
+                }
+            }
+
+            // return the image we want to preview
+            return mMatRgba;
+        } catch (RuntimeException e) {
+            return new Mat(frame.rgba().size(), frame.rgba().type(), new Scalar(0));
+        }
     }
 
+
+    private void stopCamera() {
+        if (null != mMatRgba) {
+            mMatRgba.release();
+            mMatRgba = null;
+        }
+
+        if (null != mMatGray) {
+            mMatGray.release();
+            mMatGray = null;
+        }
+
+        if (null != mFaceDetector) {
+            mFaceDetector.release();
+            mFaceDetector = null;
+        }
+
+        if (mCameraView.isConnected()) {
+            mCameraView.disconnect();
+            mCameraView.setVisibility(View.GONE);
+            mCameraView.setCvCameraViewListener((CameraBridgeViewBase.CvCameraViewListener2)null);
+        }
+    }
+
+    private void startCamera() {
+        // setup mCamera view widget
+        if (null != mCameraView) {
+            Log.i(TAG, "setup camera");
+            mCameraView.connect();                      // initialize camera
+            mCameraView.enableView();                   // turn mCamera stream on
+
+            mCameraView.setCvCameraViewListener(this);  // set listener for mCamera view callbacks
+            mCameraView.setVisibility(View.VISIBLE);    // set widget visibility on
+        }
+    }
+
+    @Override
+    public void onError(int error, Camera camera) {
+        Log.i(TAG, "onError: I caught it :-)");
+        stopCamera();
+        startCamera();
+    }
 }
